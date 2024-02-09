340ab9ed2b2fca19293fa6e1074bc40edd9f5cfd
gabsferrara <gabrielbarbosa_ferrara@hotmail.com>
2024-02-09 12:23:28 -0300

    feat: :safety_vest: finish MS to validation

--- application/usecases/mock_sender.go
+++ application/usecases/mock_sender.go
@@ -9,22 +9,15 @@ type QueueProducerMock struct {
 	queue []*domain.MessageDTO
 }
 
-type EntityMock struct {
-	Value string `json:"value"`
-}
-
 func (q *QueueProducerMock) SendMessage(msg []byte) error {
+
 	if q.queue == nil {
 		q.queue = make([]*domain.MessageDTO, 0)
 	}
 
-	var entity domain.MessageDTO
-	_ = json.Unmarshal(msg, &entity)
+	var entityDto domain.MessageDTO
+	_ = json.Unmarshal(msg, &entityDto)
 
-	q.queue = append(q.queue, &entity)
-	return nil
-}
-
-func (e EntityMock) Validate() error {
+	q.queue = append(q.queue, &entityDto)
 	return nil
 }
--- application/usecases/sender_test.go
+++ application/usecases/sender_test.go
@@ -1,20 +1,48 @@
 package usecases
 
 import (
+	"belobetty-queue-manager/domain/git"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestSenderUseCase_Exec(t *testing.T) {
 	mockSQueue := new(QueueProducerMock)
 	s := NewSenderUseCase(mockSQueue)
-	ent1 := EntityMock{Value: "Test1"}
 
-	err := s.Exec(ent1, "acao1", "funcionalidade1", "empresa1")
+	var ent1 = new(git.UserGit)
+
+	jsonData, _ := os.ReadFile("../../docs/git/users/ok.json")
+	err := json.Unmarshal(jsonData, ent1)
+	assert.Nil(t, err)
+	assert.NotNil(t, ent1)
+
+	err = s.Exec(ent1, "anyAction", "funcionalidade1", "empresa1")
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(mockSQueue.queue))
-	err = s.Exec(ent1, "acao2", "func2", "emp2")
+
+	ent2 := new(git.RepositoryGit)
+	jsonData, _ = os.ReadFile("../../docs/git/repositories/ok.json")
+	err = json.Unmarshal(jsonData, ent2)
+	err = s.Exec(ent1, "anyAction", "func2", "emp2")
+
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(mockSQueue.queue))
+
+}
+
+func TestSenderUseCase_ExecWhenErrorAndActionNotDelete(t *testing.T) {
+	mockSQueue := new(QueueProducerMock)
+	s := NewSenderUseCase(mockSQueue)
+
+	ent := new(git.RepositoryGit)
+	jsonData, _ := os.ReadFile("../../docs/git/repositories/invalidUserPermission.json")
+	err := json.Unmarshal(jsonData, ent)
+
+	err = s.Exec(ent, "anyAction", "funcionalidade1", "empresa1")
+	assert.NotNil(t, err)
+	assert.Equal(t, 0, len(mockSQueue.queue))
 
 }
